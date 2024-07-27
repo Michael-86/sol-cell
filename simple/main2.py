fba1240f568a7fb0e21c7dddb0559186103d6b71
Michael-86 <53591991+Michael-86@users.noreply.github.com>
2024-07-27 18:46:12 +0200

    Create main2.py

---
+++ simple/main2.py
@@ -0,0 +1,71 @@
+import network
+import socket
+import machine
+import utime
+
+# Connect to Wi-Fi
+ssid = 'gtfast'
+password = 'darktitan01'
+
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect(ssid, password)
+
+# Wait for connection
+max_wait = 10
+while max_wait > 0:
+    if wlan.status() < 0 or wlan.status() >= 3:
+        break
+    max_wait -= 1
+    print('waiting for connection...')
+    utime.sleep(1)
+
+if wlan.status() != 3:
+    raise RuntimeError('network connection failed')
+else:
+    print('connected')
+    status = wlan.ifconfig()
+    print('ip = ' + status[0])
+
+# Set up ADC
+adc = machine.ADC(26)
+
+# Function to read voltage
+def read_voltage():
+    raw_value = adc.read_u16()
+    voltage = (raw_value / 65535.0) * 3.3  # Convert to voltage
+    battery_voltage = voltage * (23 / 10)  # Adjust for voltage divider
+    return battery_voltage
+
+# HTML to send to browsers
+html = """<!DOCTYPE html>
+<html>
+    <head> <title>Pico W</title> </head>
+    <body> <h1>Pico W Battery Voltage</h1>
+        <p>Battery Voltage: {}</p>
+    </body>
+</html>
+"""
+
+# Open socket
+addr = socket.getaddrinfo('0.0.0.0', 90)[0][-1]
+s = socket.socket()
+s.bind(addr)
+s.listen(1)
+
+print('listening on', addr)
+
+# Listen for connections
+while True:
+    cl, addr = s.accept()
+    print('client connected from', addr)
+    request = cl.recv(1024)
+    request = str(request)
+    print('Content = {}'.format(request))
+
+    battery_voltage = read_voltage()
+    response = html.format(battery_voltage)
+
+    cl.send('HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n')
+    cl.send(response)
+    cl.close()
